84ec06d29d9e03878f48fe326c9cc941e16a97b3
Eric Ekas <eric.ekas@gmail.com>
2022-07-11 16:15:14 -0400

    mlb teams json and teams table setup

---
+++ db/migrate/20220711184845_create_teams.rb
@@ -0,0 +1,15 @@
+class CreateTeams < ActiveRecord::Migration[6.1]
+  def change
+    create_table :teams do |t|
+      t.string :full_name
+      t.string :team_code
+      t.string :city_name
+      t.string :name_short
+      t.string :league
+      t.string :division
+      t.string :division_full
+      t.string :venue_name
+      t.string :state
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,25 @@
-puts "🌱 Seeding spices..."
+require 'json'
 
-# Seed your database here
+puts "Seeding teams..."
+
+teams_json = File.read "db/mlb-teams.json"
+
+teams_hash = JSON.parse(teams_json)
+
+teams_hash['all_data'].each do |team|
+
+    Team.create(
+        full_name: team["name_display_long"],
+        team_code: team["bis_team_code"],
+        city_name: team["name_display_short"],
+        name_short: team["name_display_brief"],
+        league: team["league"],
+        division: team["division_abbrev"],
+        division_full: team["division_full"],
+        venue_name: team["venue_short"],
+        state: team["address_state"]
+        )
+
+end
 
 puts "✅ Done seeding!"
