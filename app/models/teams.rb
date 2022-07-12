a067047c0b738917556822d25dc0e7deeb4ecaa2
Eric Ekas <eric.ekas@gmail.com>
2022-07-12 16:07:40 -0400

    added games class and seed data

--- app/models/teams.rb
+++ app/models/teams.rb
@@ -1,3 +1,19 @@
 class Team < ActiveRecord::Base
-    has_many :players
+    has_many :players,
+    :primary_key => 'static_team_id',
+    :foreign_key => 'team_id'
+
+    has_many :home_games,
+    class_name: 'Game',
+    :primary_key => 'static_team_id',
+    :foreign_key => 'home_team_id'
+
+    has_many :away_games,
+    class_name: 'Game',
+    :primary_key => 'static_team_id',
+    :foreign_key => 'away_team_id'
+
+    def games
+        self.home_games + self.away_games
+    end
 end
